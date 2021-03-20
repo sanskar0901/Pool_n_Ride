bb42c330cd0f905bb8c74a1702db36a67d333f9f
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-03-20 11:57:19 +0530

    Co-authored-by: Tanishq Kashyap <Tanishq2505@users.noreply.github.com>

---
+++ js/fb.js
@@ -0,0 +1,65 @@
+const email = document.getElementById('s-email');
+const namee = document.getElementById('s-username');
+const pasword = document.getElementById('s-password');
+const mobile = document.getElementById('s-mobile');
+
+
+const database = firebase.database();
+console.log('working?')
+firebase.auth().onAuthStateChanged(function (user) {
+  console.log(user.uid)
+  if (user.uid) {
+    document.getElementById("buttuns").style.display() = "none";
+  } else {
+    document.getElementById("buttuns").style.display() = "block";
+    console.log('hi')
+  }
+});
+signup.addEventListener('click', (e) => {
+  e.preventDefault();
+  database.ref('/user/' + namee.value).set({
+    user_email: email.value,
+    user_pass: pasword.value,
+    user_mob: mobile.value,
+    user_name: namee.value
+
+  });
+
+  firebase.auth().createUserWithEmailAndPassword(email.value, pasword.value)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  alert('signed up')
+});
+
+login.addEventListener('click', (e) => {
+  const email = document.getElementById('l-email');
+  const pasword = document.getElementById('l-password');
+  e.preventDefault();
+  console.log("hi")
+  firebase.auth().signInWithEmailAndPassword(email.value, pasword.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;;
+      // ...
+
+      document.getElementById("login-a").style.display() = "none";
+      document.getElementById("loginModal").setAttribute("aria-hidden", "false")
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(email.value)
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+
+
+});
